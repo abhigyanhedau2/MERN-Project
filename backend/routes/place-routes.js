0ddc8fe5babdfd866d3725b6486974ad988d175c
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-10-23 14:27:38 +0530

    91. Adding Place-Specific Routes

--- backend/routes/place-routes.js
+++ backend/routes/place-routes.js
@@ -2,13 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous skyscrapers in the world',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+];
 
-    console.log('GET request in places');
+router.get('/:placeId', (req, res, next) => {
+
+    const placeId = req.params.placeId;
+
+    const place = DUMMY_PLACES.find(place => place.id === placeId);
 
     res.status(200).json({
         status: 'success',
-        message: 'It Works!'
+        data: {
+            place: place
+        }
     });
 
 });
