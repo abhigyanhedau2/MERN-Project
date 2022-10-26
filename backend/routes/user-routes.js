50c3454357a40a09edb8d0e63178c5a7acb63248
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-10-26 17:12:09 +0530

    166. On the Backend: Using Multer to Save Files and Filtering Files for Images Only

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const validator = require('express-validator');
 const userController = require('../controllers/user-controllers');
+const fileUpload = require('../middleware/file-upload');
 const router = express.Router();
 
 const signUpValidationArr = [
@@ -13,6 +14,6 @@ const { getUsers, signup, login } = userController;
 
 router.get('/', getUsers);
 router.post('/login', login);
-router.post('/signup', signUpValidationArr, signup);
+router.post('/signup', fileUpload.single('image'), signUpValidationArr, signup);
 
 module.exports = router;
